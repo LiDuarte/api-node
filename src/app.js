72eb7aa1d5b2c8be340218d95e264460bf53f56c
Eliasafe <li_okay@hotmail.com>
2017-09-20 18:27:01 -0300

    Primeiro commit Node.js

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+
+    // carregando rotas
+const indexRoute = require('./routes/index-route');
+const productsRoute = require('./routes/products-route');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+             extended: false 
+    }));
+
+
+
+app.use('/', indexRoute);
+app.use('/products', productsRoute);
+
+
+module.exports = app;
